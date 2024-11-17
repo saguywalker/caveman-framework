f7bb69a7f65b63fba1ab24de6a3d3069539ff4cf
saguywalker <saguywalker@protonmail.com>
2024-11-17 20:29:13 +0900

    cave insert trigger migration

---
+++ migrations/scripts/20241117111616_cave_insert_trigger.sql
@@ -0,0 +1,26 @@
+-- // cave_insert_trigger
+-- Migration SQL that makes the change goes here.
+CREATE OR REPLACE FUNCTION prehistoric_cave_insert_function()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO proletarian.job(
+        job_type, payload
+    )
+    VALUES (
+        ':prehistoric.cave/insert', row_to_json(NEW)
+    );
+    return NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prehistoric_cave_insert_trigger
+AFTER INSERT ON prehistoric.cave
+FOR EACH ROW
+EXECUTE PROCEDURE prehistoric_cave_insert_function();
+
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+DROP TRIGGER prehistoric_cave_insert_trigger ON prehistoric.cave;
+DROP FUNCTION prehistoric_cave_insert_function;
+
