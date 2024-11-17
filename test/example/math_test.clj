f529a1966dbe6ca1971e99495f701053b5ff8e9b
saguywalker <saguywalker@protonmail.com>
2024-11-17 21:22:08 +0900

    add test

--- test/example/math_test.clj
+++ test/example/math_test.clj
@@ -1,5 +1,17 @@
 (ns example.math-test
-  (:require [clojure.test :as t]))
+  (:require [clojure.test :as t]
+            [example.test-system :as test-system]
+            [next.jdbc :as jdbc]))
 
 (t/deftest one-plus-one
   (t/is (= (+ 1 1) 2) "One plus one equals 3!"))
+
+(t/deftest counting-works
+  (test-system/with-test-db
+    (fn [db]
+      (jdbc/execute! db ["INSERT INTO prehistoric.hominid(name) VALUES(?)" "Grunto"])
+      (jdbc/execute! db ["INSERT INTO prehistoric.hominid(name) VALUES(?)" "Blingus"])
+      (let [res (jdbc/execute-one! db ["SELECT COUNT(*) as count FROM prehistoric.hominid"])]
+        (println res))
+      (t/is (= (:count (jdbc/execute-one! db ["SELECT COUNT(*) as count FROM prehistoric.hominid"]))
+               2)))))
